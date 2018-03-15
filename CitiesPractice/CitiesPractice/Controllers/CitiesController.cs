2e1edd24f7bbcd202875bcceb1b4be0823f368c6
perfectlyodd <37376799+perfectlyodd@users.noreply.github.com>
2018-03-15 10:20:59 -0500

    Add files via upload

---
+++ CitiesPractice/CitiesPractice/Controllers/CitiesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitiesPractice
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        /* Old dummy code.  To do: replace this comment by an "obsolete" attribute.
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id = 1, Name = "New York City" },
+                new { id = 2, Name = "Antwerp" }
+            });
+        }
+        */
+    }
+}
---
+++ CitiesPractice/CitiesPractice/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CitiesPractice
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            //app.UseDeveloperExceptionPage();
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                
+            } 
+            else
+            {
+                app.UseExceptionHandler();
+            } 
+
+            app.UseMvc();
+            
+            /* Example: Throws an exception to illustrate error page 
+            app.Run((context) =>
+            {
+                throw new Exception("Example exception");
+            });
+            */
+            
+            /* Example: Typical "Hello World" prototype 
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+            */
+        }
+    }
+}
